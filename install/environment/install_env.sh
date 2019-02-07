2ee0c8292e8a48fb70f0f52a5d85194dd0a1d70b
Suhas Gundimeda <snugghash@gmail.com>
2019-02-06 18:28:01 -0800

    This works? Add openjdk8 purge and install spark

--- install/environment/install_env.sh
+++ install/environment/install_env.sh
@@ -13,25 +13,28 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+sudo dpkg --configure -a
 
 sudo add-apt-repository ppa:openjdk-r/ppa -y
-
+wait
 sudo apt-get update
+wait
+sudo apt-get --yes install ssh rsync scala python-dev python-pip python-numpy python-scipy python-pandas gfortran git supervisor ruby bc python3 python3-pip python3-setuptools
+#sudo dpkg --configure -a
+wait
+#pip install --upgrade pip
 
-sudo apt-get --yes install ssh rsync openjdk-8-jdk scala python-dev python-pip python-numpy python-scipy python-pandas gfortran git supervisor ruby bc python3 python3-pip python3-setuptools\
-	udev\
-	libprocps4\
-	perl\
-	man-db\
-	libc-dev-bin\
-	libc6-dev
-sudo dpkg --configure -a
-pip install --upgrade pip
+
+sudo apt-get remove --purge openjdk* java-common default-jdk
+sudo apt-get autoremove --purge
+sudo apt-get install openjdk-8-jdk
+#sudo dpkg --configure -a
 
 # get sbt repository
 wget https://dl.bintray.com/sbt/debian/sbt-0.13.7.deb -P ~/Downloads
 sudo dpkg -i ~/Downloads/sbt-*
-sudo dpkg --configure -a
+#sudo dpkg --configure -a
+wait
 
 # get maven3 repository
 #sudo apt-get purge maven maven2 maven3
@@ -42,14 +45,20 @@ sudo dpkg --configure -a
 
 sudo update-java-alternatives -s java-1.8.0-openjdk-amd64
 
-sudo pip install nose seaborn boto scikit-learn "ipython[notebook]==5.5.0"
+
+
+sudo pip install nose seaborn boto scikit-learn
+#sudo pip install "ipython[notebook]==5.5.0"
 
 if ! grep "export JAVA_HOME" ~/.profile; then
   echo -e "\nexport JAVA_HOME=/usr" | cat >> ~/.profile
   echo -e "export PATH=\$PATH:\$JAVA_HOME/bin" | cat >> ~/.profile
 fi
+wait
 
 sudo rm /var/lib/dpkg/lock
-sudo dpkg --configure -a
+#sudo dpkg --configure -a
+wait
 sudo apt-get --yes install bc
-sudo dpkg --configure -a
+wait
+#sudo dpkg --configure -a
